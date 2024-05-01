27b04668b2b43456dd37331bb04f2c673aaeff6f
nijatfg <153588579+nijatfg@users.noreply.github.com>
2024-05-01 22:29:23 +0400

    first commit

---
+++ src/test/java/az/code/codehackathon/CodeHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package az.code.codehackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
